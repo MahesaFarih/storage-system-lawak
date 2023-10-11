490b364c6394ad98109d7b1e96a4d6feab1dab1f
D.Generate <mahesafarih7@gmail.com>
2023-10-11 14:46:46 +0700

    Tugas 6

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, edit_product,show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, count_item
+from main.views import get_product_json,add_product_ajax,show_main, create_product, edit_product,show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, count_item
 
 app_name = 'main'
 
@@ -15,5 +15,7 @@ urlpatterns = [
     path('login/', login_user, name='login'), 
     path('logout/', logout_user, name='logout'),
     path('count_item/', count_item, name='count_item'),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
 
 ]
